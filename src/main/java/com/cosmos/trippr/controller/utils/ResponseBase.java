16932cee99cbf08cbf03afc8387d1bd13bf1c0b8
naveenporter <138476783+naveenporter@users.noreply.github.com>
2024-02-19 00:25:26 +0530

    [Feat] Add ResponseBase for request Response Structure

---
+++ src/main/java/com/cosmos/trippr/controller/utils/ResponseBase.java
@@ -0,0 +1,21 @@
+package com.cosmos.trippr.controller.utils;
+
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Data
+public class ResponseBase {
+
+  public static ResponseEntity<Object> generateResponse(HttpStatus status, Object data, String message) {
+    Map<String, Object> map = new HashMap<>();
+    map.put("status", status);
+    map.put("data", data);
+    map.put("message", message);
+    return new ResponseEntity<Object>(map, status);
+  }
+}
